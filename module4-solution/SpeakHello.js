e0d2afe3a4a2974404b86858206b16f4052b08e6
fbab920 <fbab920@aucklanduni.ac.nz>
2022-02-06 20:06:48 +1300

    :sparkles: complete tasks

---
+++ module4-solution/SpeakHello.js
@@ -0,0 +1,13 @@
+(function (window) {
+  var helloSpeaker = {};
+
+  // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
+  var speakWord = "Hello";
+
+  helloSpeaker.speak = function speak(name) {
+    console.log(speakWord + " " + name);
+  }
+
+  window.helloSpeaker = helloSpeaker;
+
+})(window);
